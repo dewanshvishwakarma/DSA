da7f8d94d9b3fd0969c4786d91ba608e43def9e5
Devansh <dewanshvishwakarma073@gmail.com>
2026-03-15 20:13:47 +0530

    daily dsa code

---
+++ src/CheckSorted.java
@@ -0,0 +1,21 @@
+public class CheckSorted {
+    static boolean sorted(int[] a){
+        boolean check=true;
+        for(int i=0;i<a.length-1;i++){
+            if (a[i]>a[i+1]) {
+                check = false;
+                break;
+            }
+        }
+        return check;
+    }
+    public static void main(String[] args){
+        int[] a={1,2,3,4,5,6};
+        boolean output=sorted(a);
+        if (output){
+            System.out.println("yes array is sorted ");
+        }else{
+            System.out.println("array is not sorted ");
+        }
+    }
+}
---
+++ src/CountEvenOdd.java
@@ -0,0 +1,22 @@
+public class CountEvenOdd {
+    static int[] Count(int[] arr){
+        int n=arr.length;
+        int E_count=0;
+        int O_count=0;
+        for (int i=0;i<n;i++){
+            if (arr[i]%2==0){
+                E_count++;
+            }else {
+                O_count++;
+            }
+        }
+        return new int[]{E_count,O_count};
+    }
+
+    public static void main(String[] args){
+        int[] a={2,4,6,8};
+        int[] output=Count(a);
+        System.out.println("even number are " + output[0]);
+        System.out.println("odd number are " + output[1]);
+    }
+}
