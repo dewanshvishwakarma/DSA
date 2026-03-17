d92dcb0b5c44514aa7d99289f9f33296c64357b6
Devansh <dewanshvishwakarma073@gmail.com>
2026-03-18 00:38:41 +0530

    daily dsa code

--- src/MoveZero.java
+++ src/MoveZero.java
@@ -1,16 +1,27 @@
+
 public class MoveZero {
+    static void Swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+
     static void Move(int[] nums){
-        int z=0;
-        int nz=0;
         int n= nums.length;
-
-        while (nz<n){
-            if (nums[nz]!=0){
-
+        int non_Zero=0;
+        for (int i=0;i<n;i++){
+            if(nums[i]!=0-){
+                Swap(nums,i,non_Zero);
+                non_Zero++;
             }
         }
     }
     public static void main(String[] args){
-
+        int[] a={1,0,4,0,5,0,4,0,0};
+        Move(a);
+        for(int i=0;i< a.length;i++){
+            System.out.print(a[i]+" ");
+        }
     }
 }
