7875030c6ccc91ba307ce237b706bba3241d19e9
Devansh <dewanshvishwakarma073@gmail.com>
2026-04-10 22:33:25 +0530

    daily dsa 10 april

--- src/arrays/remove_element.java
+++ src/arrays/remove_element.java
@@ -15,9 +15,28 @@ public class remove_element {
         }
         return k;
     }
+    
+    static int Remove1(int[] a , int val){
+        int n=a.length;
+        int j=n-1;
+        int i=0;
+        while (i<j){
+            if (a[i]==val){
+                a[i]=a[j-1];
+                j--;
+            }else {
+                i++;
+            }
+        }
+        return j;
+    }
    public static void main(String[] args) {
-    int[] arr={1,2,3,3,2,7};
+    int[] arr={1,2,3,3};
     int result=Remove(arr,2);
        System.out.println(result);
+
+
+    int result2=Remove1(arr,2);
+       System.out.println(result2);
     }
 }
