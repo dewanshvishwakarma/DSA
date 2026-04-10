80f1e3ba7219639b653edec45703ae5e464878cb
Devansh <dewanshvishwakarma073@gmail.com>
2026-04-10 15:23:38 +0530

    daily dsa code 10 april(exception handling)

--- src/OOPS_IN_JAVA/Exception1.java
+++ src/OOPS_IN_JAVA/Exception1.java
@@ -4,12 +4,12 @@ public class Exception1 {
    public static void main(String[] args) {
         try {
             int a=10;
-            int b=100;
+            int b=0;
             int c=a/b;
             System.out.println(c);
             System.out.println("1");
         }
-        catch (Exception a){
+        catch (Exception e){
             System.out.println("devide by zero error ");
         }
 
