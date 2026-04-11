2650d446c7fd075428ea92e6224fd78a63e4dfcd
Devansh <dewanshvishwakarma073@gmail.com>
2026-04-11 19:05:10 +0530

    dsa daily  11 april

---
+++ src/BinarySearch.java
@@ -0,0 +1,27 @@
+public class BinarySearch {
+    public static int binary(int[] a, int key){
+        int l=0;
+        int h=a.length-1;
+        int mid =0;
+
+        while (l<=h){
+            mid=(l+h)/2;
+
+            if (key==a[mid]){
+                return mid;
+            }
+            else if (key<a[mid]){
+                h=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return -1;
+    }
+    static void main(String[] args) {
+        int[] a={2,4,6,8,10,12,14,16};
+        int result=binary(a,11);
+        System.out.println(result);
+    }
+}
