d1243df57136706b321816a1a9328954dbfa4f35
Devansh <dewanshvishwakarma073@gmail.com>
2026-04-16 23:09:22 +0530

    inheritance in java 16 april

--- src/cybromeJAVA/inheritance2.java
+++ src/cybromeJAVA/inheritance2.java
@@ -1,19 +1,31 @@
 package cybromeJAVA;
 class parent{
+
+    parent(){
+        System.out.println(" hellp parent");
+    }
     void m1(){
         System.out.println("parent class");
     }
 }
+
 class child extends parent{
+
+    child(){
+        System.out.println("child");
+    }
     void m2(){
         System.out.println("child class");
     }
 }
 public class inheritance2 {
-    static void main(String[] args) {
-        child c1=new child();
-        parent p=new child();
-        p.m1();
+   public static void main(String[] args) {
+        child c=new child();
+
+
+//        child c1=new child();
+//        parent p=new child();
+//        p.m1();
 //        p.m2() no child method class
     }
 }
