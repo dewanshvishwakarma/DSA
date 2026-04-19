1cb5f866bc89d1e5334afa0b7d38cc227e4f8635
Devansh <dewanshvishwakarma073@gmail.com>
2026-04-19 12:57:43 +0530

    simple String palindrome check(two pointer pattern)

---
+++ src/String/Check_palindrome1.java
@@ -0,0 +1,30 @@
+package String;
+
+public class Check_palindrome1 {
+//    for simple string
+    static boolean check(String s){
+        int i=0;
+        int j=s.length()-1;
+
+        while (i<j){
+            if (s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+    String name ="madam";
+    boolean result=check(name);
+        System.out.println(result);
+
+    if (check(name)){
+        System.out.println("yes palindrome");
+    }
+    else{
+        System.out.println("not palindrome");
+    }
+    }
+}
