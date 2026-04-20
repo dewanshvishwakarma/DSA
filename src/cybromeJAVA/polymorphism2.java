daafb047ca1526c543b4d6e93d9b63e3ffdbb794
Devansh <dewanshvishwakarma073@gmail.com>
2026-04-20 22:35:52 +0530

    polymorphism in java

--- src/cybromeJAVA/polymorphism2.java
+++ src/cybromeJAVA/polymorphism2.java
@@ -1,20 +1,18 @@
-package cybromeJAVA;
-class p{
-    void m1(int a){
-        System.out.println("m1");
-    }
-}
-
-class c extends p{
-    void m2(int a,int b){
-        System.out.println("m2");
-    }
-    void m3(int a,int b,int c){
-        System.out.println("m3");
-    }
-}
-public class polymorphism2 {
-    c c1=new c();
-    p p2=new c();
-    p2.m1(10);
-}
+//package cybromeJAVA;
+//class p{
+//    void m1(int a){
+//        System.out.println("m1");
+//    }
+//}
+//
+//class c extends p{
+//    void m2(int a,int b){
+//        System.out.println("m2");
+//    }
+//    void m3(int a,int b,int c){
+//        System.out.println("m3");
+//    }
+//}
+//public class polymorphism2 {
+//    c c1=new c();
+//    p p2=new c();
