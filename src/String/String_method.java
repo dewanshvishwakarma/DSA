e7093f9d0f6fcb3cfc22b26f0c0f275748488026
Devansh <dewanshvishwakarma073@gmail.com>
2026-04-20 22:25:18 +0530

    learn string method

---
+++ src/String/String_method.java
@@ -0,0 +1,13 @@
+package String;
+
+public class String_method {
+    static void main(String[] args) {
+        String name="   dewa  nsh";
+        String name2="";
+        System.out.println(name.length());
+        System.out.println(name.trim());
+        System.out.println(name.trim().length());
+        System.out.println(name2.isEmpty());
+
+    }
+}
