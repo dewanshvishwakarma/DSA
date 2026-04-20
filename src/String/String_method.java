2f84da1b5a4e34b04c7fc1fcd9334a4c4bae4656
Devansh <dewanshvishwakarma073@gmail.com>
2026-04-20 22:50:04 +0530

    string methods

--- src/String/String_method.java
+++ src/String/String_method.java
@@ -4,10 +4,13 @@ public class String_method {
     static void main(String[] args) {
         String name="   dewa  nsh";
         String name2="";
+        String s1="sanjana";
+        String s2="sanjana";
         System.out.println(name.length());
         System.out.println(name.trim());
         System.out.println(name.trim().length());
         System.out.println(name2.isEmpty());
+        System.out.println(s1.equals(s2));
 
     }
 }
