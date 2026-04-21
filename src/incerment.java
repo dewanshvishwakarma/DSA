727b1339105fdae0dbf0483ad087bf230ffb6a22
Devansh <dewanshvishwakarma073@gmail.com>
2026-04-21 21:38:18 +0530

    daily java code

---
+++ src/incerment.java
@@ -0,0 +1,7 @@
+public class incerment {
+    static void main() {
+        int number=10;
+        System.out.println(number++ + ++number);
+        System.out.println(++number + number++);
+    }
+}
