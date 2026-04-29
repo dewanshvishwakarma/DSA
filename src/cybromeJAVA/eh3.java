c7e2a911cbfa3a8a8a434be2cfa4a1d83cc63635
Devansh <dewanshvishwakarma073@gmail.com>
2026-04-30 00:21:19 +0530

    exception handling with throw

---
+++ src/cybromeJAVA/eh3.java
@@ -0,0 +1,10 @@
+package cybromeJAVA;
+
+public class eh3 {
+    static void main(String[] args) {
+        System.out.println("welcome to java the customised java class ");
+        System.out.println(10/2);
+        throw new ArithmeticException();
+        
+    }
+}
