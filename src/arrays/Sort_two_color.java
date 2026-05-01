ed772c94b4a77bcb03748887b87f99220f43d2bb
Devansh <dewanshvishwakarma073@gmail.com>
2026-05-01 15:21:51 +0530

    sort two color 0 1 with two pointer pattern

--- src/arrays/Sort_two_color.java
+++ src/arrays/Sort_two_color.java
@@ -1,7 +1,7 @@
 package arrays;
 
 public class Sort_two_color {
-//    apply brute force , with the help of count , count the 0 and 1 and fill the array
+//   approach:1 apply brute force , with the help of count , count the 0 and 1 and fill the array
     static void Sort_color1(int[] a){
         int n=a.length;
         int count0=0;
@@ -18,7 +18,7 @@ public class Sort_two_color {
             }
         }
     }
-//    apply optimal approach two pointer i and j are in first if j=0; swap them and i++
+//   approach:2 apply optimal approach two pointer i and j are in first if j=0; swap them and i++
     static void Sort_color2(int[] a){
         int i=0;
         for (int j=0;j< a.length;j++){
@@ -30,7 +30,7 @@ public class Sort_two_color {
             }
         }
         }
-
+//  with two pointer approach:3
      static void Sort_color3(int[] a){
         int n=a.length;
         int i=0;
@@ -47,12 +47,6 @@ public class Sort_two_color {
             }
         }
      }
-
-
-
-
-
-
     static void print(int[] a){
         for (int i=0;i<a.length;i++){
             System.out.println(a[i]+ " ");
