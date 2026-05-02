ce4877952240456fd1a2e834c6b0e3229d01b0c9
Devansh <dewanshvishwakarma073@gmail.com>
2026-05-02 06:49:25 +0530

    create String  understand SCP area

---
+++ src/cybromeJAVA/string1.java
@@ -0,0 +1,17 @@
+package cybromeJAVA;
+// string is an class in java ,there are two method to create string ,
+//with the help of (new) and with the help of ("")
+public class string1 {
+    static void main(String[] args) {
+//     with the help of new
+//        create object in heap area with refrence vriable s1 and s2
+        String s1=new String("hello sir ");
+        String s2=new String("how are you");
+
+//        with the help of ""
+//        create object in SCP string constant pool inside heap area
+        String s3="i am fine my child";
+        String s4="sanjana";
+
+    }
+}
