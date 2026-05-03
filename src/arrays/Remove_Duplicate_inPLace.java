65c36a0f4c8db60adb1bf54381f7370520aab2d8
Devansh <dewanshvishwakarma073@gmail.com>
2026-05-03 22:19:41 +0530

    remove duplicate in sorted array

--- src/arrays/Remove_Duplicate_inPLace.java
+++ src/arrays/Remove_Duplicate_inPLace.java
@@ -13,9 +13,26 @@ public class Remove_Duplicate_inPLace {
         return j+1;
     }
 
+    static int Duplicate2(int[] a){
+        int n=a.length;
+        int i=0;
+        int j=1;
+        while (j<n){
+            if (a[i]==a[j]){
+                j++;
+            }
+            else{
+                i++;
+                a[i]=a[j];
+                j++;
+            }
+        }
+        return i+1;
+    }
+
     public static void main(String[] args){
         int[] a={2,2,3,3,4,6,6,11,11,11,12,13,};
-        int index=Duplicate(a);
+        int index=Duplicate2(a);
         for (int i=0;i<index;i++){
             System.out.println(a[i]);
         }
