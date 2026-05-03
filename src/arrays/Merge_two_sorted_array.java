58c4bd4c25c28386a64b76bb3acb25c1e69d68a4
Devansh <dewanshvishwakarma073@gmail.com>
2026-05-03 17:05:44 +0530

    mrege two Array type2

--- src/arrays/Merge_two_sorted_array.java
+++ src/arrays/Merge_two_sorted_array.java
@@ -18,6 +18,40 @@ public class Merge_two_sorted_array {
             }
         }
     }
+//    type 2
+
+    static int[]  MergeArray(int[] a,int[] b){
+        int m= a.length;
+        int n=b.length;
+        int i=0;
+        int j=0;
+        int[] result =new int[m+n];
+        int k=0;
+
+        while (i<m && j<n){
+            if (a[i]<=b[j]){
+                result[k]=a[i];
+                i++;
+                k++;
+            }else {
+                result[k]=b[j];
+                j++;
+                k++;
+            }
+        }
+        while (i<a.length){
+            result[k]=a[i];
+            k++;
+            i++;
+        }
+
+        while (j<b.length){
+            result[k]=b[j];
+            k++;
+            j++;
+        }
+        return result;
+    }
 //    print
     static void print(int[] a){
         for (int i=0;i<a.length;i++) {
@@ -25,9 +59,14 @@ public class Merge_two_sorted_array {
         }
     }
     public static void main(String[] args){
-        int[] a1={1,3,5,0,0,0};
-        int[] b1={2,4,6};
-        AddSorted1(a1,b1,3,3);
-        print(a1);
+//        int[] a1={1,3,5,0,0,0};
+//        int[] b1={2,4,6};
+//        AddSorted1(a1,b1,3,3);
+//        print(a1);
+
+        int[] a={1,3,5};
+        int[] b={2,4,6,8};
+        int[] result =MergeArray(a,b);
+        print(result);
     }
 }
