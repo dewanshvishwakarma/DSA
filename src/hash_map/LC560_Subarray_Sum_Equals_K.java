7560f760b23863a9cd169321d1ff7035104bd37c
Devansh <dewanshvishwakarma073@gmail.com>
2026-06-16 20:32:45 +0530

    solve(lc560) subarray sum equal to k using hashmap+prefix sum

---
+++ src/hash_map/LC560_Subarray_Sum_Equals_K.java
@@ -0,0 +1,29 @@
+package hash_map;
+
+import java.util.HashMap;
+
+//LC=https://leetcode.com/problems/subarray-sum-equals-k/description/
+public class LC560_Subarray_Sum_Equals_K {
+
+    static int sub_array(int[] a,int k){
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int count=0;
+        int prefix_sum=0;
+        map.put(0,1);
+        for (int i=0;i<a.length;i++){
+            prefix_sum=prefix_sum+a[i];
+            int dif=prefix_sum-k;
+
+            if (map.containsKey(dif)){
+                count=count+map.get(dif);
+            }
+            map.put(prefix_sum,map.getOrDefault(prefix_sum,0)+1);
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+int[] a={1,2,3};
+int r=sub_array(a,3);
+        System.out.println("count of sub array is " + r);
+    }
+}
