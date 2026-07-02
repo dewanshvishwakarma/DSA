996c8759cf5d475636eb38bd6e9d77a7b9baced0
Devansh <dewanshvishwakarma073@gmail.com>
2026-07-02 23:34:13 +0530

    deeply understand the for loop by solving pattern problem solving

--- src/pattern/traingle.java
+++ src/pattern/traingle.java
@@ -6,6 +6,7 @@ public class traingle {
     public static void main(String[] args) {
 
         Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
 
         for (int i = n; i >= 1; i--) {
 
