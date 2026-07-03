1b8828bdc6238f49a207a0d8f86b4e7c0843f9f1
Devansh <dewanshvishwakarma073@gmail.com>
2026-07-03 20:33:48 +0800

    solve gfg two sum, return true if find the target pair

---
+++ src/hash_map/GFG_Two_Sum_Pair_with_Given_Sum.java
@@ -0,0 +1,25 @@
+package hash_map;
+
+import java.util.HashMap;
+
+public class GFG_Two_Sum_Pair_with_Given_Sum {
+   static boolean twoSum(int[] a, int target){
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        for (int i=0;i<a.length;i++){
+            int c=target-a[i];
+            if (map.containsKey(c)){
+                return true;
+            }else {
+                map.put(a[i],i);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] a={1, -2, 1, 0, 5};;
+        boolean res=twoSum(a,0);
+        System.out.println(res);
+    }
+}
