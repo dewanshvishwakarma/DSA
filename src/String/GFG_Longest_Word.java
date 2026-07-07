777b6e77ff40a7d442f440b54029530e2d65f9a1
Devansh <dewanshvishwakarma073@gmail.com>
2026-07-07 22:01:50 +0530

    solve GFG (basic) problem longest String in String array

--- src/String/GFG_Longest_Word.java
+++ src/String/GFG_Longest_Word.java
@@ -2,8 +2,8 @@ package String;
 //Given an array arr[] containing strings of names.
 // Your task is to return the longest string.
 // If there are multiple names of the longest size, return the first occurring name.
+//https://www.geeksforgeeks.org/problems/display-longest-name0853/1?page=2&category=Arrays,Java&difficulty=Basic&sortBy=submissions
 
-//
 public class GFG_Longest_Word {
     public String longest(String[] arr){
         String ans=arr[0];
