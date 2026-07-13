a4d6a9486d807d667628f4756afa2f79c4e5a785
Devansh <dewanshvishwakarma073@gmail.com>
2026-07-13 23:40:43 +0530

    pattern printing to improve looping logic star bala kar liya yeeeeeeeeeeeeeeee............

---
+++ src/pattern/inverted_star_pyramid.java
@@ -0,0 +1,18 @@
+package pattern;
+
+public class inverted_star_pyramid {
+    static void pattern(int n) {
+        for (int i=n; i>=1; i--) {
+            for (int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for (int k=1;k<=2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+pattern(5);
+    }
+}
