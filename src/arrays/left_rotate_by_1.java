5a01da226e4e9049c448c7e259a0dd6cb6870956
Devansh <dewanshvishwakarma073@gmail.com>
2026-09-04 12:24:33 +0530

    now right rotate by one

--- src/arrays/left_rotate_by_1.java
+++ src/arrays/left_rotate_by_1.java
@@ -1,4 +1,20 @@
 package arrays;
 
 public class left_rotate_by_1 {
+    static void left_rotate_one(int[] a){
+        int n= a.length;
+        int first=a[0];
+        for (int i=1;i<n;i++){
+            a[i-1]=a[i];
+        }
+        a[n-1]=first;
+    }
+    public static void main(String[] args) {
+        int[] a={10,20,30,40,50};
+        left_rotate_one(a);
+        for (int i=0;i<a.length;i++){
+            System.out.println(a[i]  + " ");
+        }
+    }
+
 }
