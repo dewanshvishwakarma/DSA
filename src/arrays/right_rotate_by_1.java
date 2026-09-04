0f555080ea69cb18b466121e353cadc9ecf89a1d
Devansh <dewanshvishwakarma073@gmail.com>
2026-09-04 12:05:37 +0530

    solve simple right rotate by one understand reverse looping concept

--- src/arrays/right_rotate_by_1.java
+++ src/arrays/right_rotate_by_1.java
@@ -1,5 +1,17 @@
 package arrays;
 
 public class right_rotate_by_1 {
+    static void rotateRightOne(int[] a){
+        int n=a.length;
+        int last=a[n-1];
+        for (int i=n-2;i>=0;i--){
+            a[i]=a[i+1];
+        }
+        a[0]=last;
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
