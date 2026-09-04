bd432cd78ffbad82b8c3d2d58d0e233d091cb697
Devansh <dewanshvishwakarma073@gmail.com>
2026-09-04 11:59:14 +0530

    find out the last word length in a String using the char comparision

--- src/String/last_word_in_String.java
+++ src/String/last_word_in_String.java
@@ -1,5 +1,26 @@
 package String;
 
 public class last_word_in_String {
+    static int findLength(String s){
+        int n=s.length();
+        int i=n-1;
+        int res=0;
+        while (i>0){
+            char ch=s.charAt(i);
+            if (ch==' '){
+                break;
+            }else{
+                res++;
+                i--;
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args){
+        String s="i love you sanjana";
+        int ans=findLength(s);
+        System.out.println(ans);
+
+    }
 
 }
