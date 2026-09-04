b8225629a42d003b6f6b40e685c5397f52cb55c9
Devansh <dewanshvishwakarma073@gmail.com>
2026-09-04 12:58:57 +0530

    rotate array k times at left by reverse approach

--- src/arrays/rotate_k_by_left.java
+++ src/arrays/rotate_k_by_left.java
@@ -1,4 +1,23 @@
 package arrays;
 
 public class rotate_k_by_left {
+    static void reverse(int[] a,int s,int e){
+        while (s<=e){
+        int temp=a[s];
+        a[s]=a[e];
+        a[e]=temp;
+        s++;
+        e--;
+        }
+    }
+    static void rotate(int[] a ,int k){
+        int n=a.length;
+        reverse(a,0,k-1);
+        reverse(a,k,n-1);
+        reverse(a,0,n-1);
+
+    }
+    public static void main(String[] args) {
+
+    }
 }
