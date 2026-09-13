2a0db069ce3d8391d550cf1ceaa79faebf8497b5
Devansh <dewanshvishwakarma073@gmail.com>
2026-09-13 06:10:12 +0530

    first unique character in String leetcode 387 (HashMap)

---
+++ src/hash_map/first_unique_character387.java
@@ -0,0 +1,56 @@
+package hash_map;
+
+import java.util.HashMap;
+
+// using the hashMAp
+public class first_unique_character387 {
+    static int FUC(String s){
+        HashMap<Character,Integer> map=new HashMap<>();
+        int n=s.length();
+
+        for (int i=0;i<n;i++){
+            char ch =s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        for (int i=0;i<n;i++){
+            char ch2=s.charAt(i);
+            if (map.get(ch2)==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //use frequency array
+
+    static int FUC1(String s){
+        int n=s.length();
+        int[] ans=new int[26];
+        for (int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            int index=ch-'a';
+            ans[index]=ans[index]+1;
+        }
+        for (int i=0;i<n;i++){
+            char ch2=s.charAt(i);
+            int index2=ch2-'a';
+            if (ans[index2]==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        String s="dee";
+        int result=FUC1(s);
+        System.out.println("first unique character is " + result );
+
+    }
+
+    //Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+//leetcode 387
+//https://leetcode.com/problems/first-unique-character-in-a-string/description/
+
+}
