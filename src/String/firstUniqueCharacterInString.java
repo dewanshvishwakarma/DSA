a536de61e6b2cbaca73b79676f90053bfb28af52
Devansh <dewanshvishwakarma073@gmail.com>
2026-09-13 06:03:31 +0530

    first unique character in String leetcode 387 use array

--- src/String/firstUniqueCharacterInString.java
+++ src/String/firstUniqueCharacterInString.java
@@ -2,6 +2,7 @@ package String;
 import java.util.HashMap;
 //Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 //leetcode 387
+//https://leetcode.com/problems/first-unique-character-in-a-string/description/
 
 
 public class firstUniqueCharacterInString {//using the hashmap to solve
