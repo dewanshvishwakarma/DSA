832810bdb653f5c18442d02e1f1d498502753f2b
Devansh <dewanshvishwakarma073@gmail.com>
2026-09-14 22:50:13 +0530

    leetcode 28 first occurrence in string easy using two pointer

--- src/String/index_of_first_occueranec28.java
+++ src/String/index_of_first_occueranec28.java
@@ -24,3 +24,13 @@ public class index_of_first_occueranec28 {
         System.out.println(result);
     }
 }
+//Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+//
+//
+//
+//Example 1:
+//
+//Input: haystack = "sadbutsad", needle = "sad"
+//Output: 0
+//Explanation: "sad" occurs at index 0 and 6.
+//The first occurrence is at index 0, so we return 0.
