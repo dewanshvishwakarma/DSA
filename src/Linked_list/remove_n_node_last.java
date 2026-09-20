b2884ad35ce40538e563c944091a284c1a215806
Devansh <dewanshvishwakarma073@gmail.com>
2026-09-20 16:15:54 +0530

    remove n th node from last using the dummy node

--- src/Linked_list/remove_n_node_last.java
+++ src/Linked_list/remove_n_node_last.java
@@ -1,6 +1,24 @@
 package Linked_list;
 
 public class remove_n_node_last {
+    static public Node remove(Node head,int n){
+        Node dummy=new Node(0);
+        dummy.next=head;
+        Node f=dummy;
+        Node s=dummy;
+        for (int i=0;i<n;i++){
+            f=f.next;
+        }
+
+        while (f.next!=null){
+            f=f.next;
+            s=s.next;
+        }
+        s.next=s.next.next;
+
+        return dummy.next;
+    }
+
     public Node removeNthFromEnd(Node head,int n){
         int len=0;
         Node temp1=head;
@@ -18,6 +36,29 @@ public class remove_n_node_last {
         }
         temp.next=temp.next.next;
         return head;
+
+    }
+
+    static void print (Node temp){
+        while (temp!=null){
+            System.out.print(temp.data + " ");
+            temp=temp.next;
+        }
+    }
+
+    public static void main(String[] a){
+        Node head=new Node(10);
+        Node b=new Node(20);
+        Node c=new Node(30);
+        Node d=new Node(500);
+
+        head.next=b;
+        b.next=c;
+        c.next=d;
+
+        Node r=remove(head,1);
+        print(r);
+
 
     }
 }
--- src/String/add_binary67.java
+++ src/String/add_binary67.java
@@ -1,22 +1,23 @@
-package String;
-
-public class add_binary67 {
-    public String addBinary(String a, String b){
-        int i=a.length();
-        int j=b.length();
-        int carry=0;
-
-        while (i>=0 && j>=0 && carry!=0){
-            int f=a.charAt(i);
-            int s=b.charAt(j);
-            int sum=f+s;
-            if (sum==10){
-                carry=1;
-                int result=sum%10;
-            }
-        }
-    }
-    public static void main(String[] args){
-
-    }
-}
+//package String;
+//
+//public class add_binary67 {
+//    public String addBinary(String a, String b){
+//        int i=a.length();
+//        int j=b.length();
+//        int carry=0;
+//
+//        while (i>=0 && j>=0 && carry!=0){
+//            int f=a.charAt(i);
+//            int s=b.charAt(j);
+//            int sum=f+s;
+//            if (sum==10){
+//                carry=1;
+//                int result=sum%10;
+//            }
+//        }
+//        return re
+//    }
+//    public static void main(String[] args){
+//
+//    }
+//}
